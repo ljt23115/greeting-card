dcb6244cfbaf2e01b825441377cbe705252b953a
tsja2001 <2945532754@qq.com>
2023-03-02 00:29:30 +0800

    feat: 完成部分视频功能

--- src/views/home/children/video/index.jsx
+++ src/views/home/children/video/index.jsx
@@ -1,10 +1,115 @@
-import React, { memo } from 'react'
+import React, { memo, useEffect, useRef, useState } from 'react'
 import VideoWrapper from './style'
 
 const Video = memo(() => {
+  const videoRef = useRef(null)
+  // 播放/暂停
+  const [playing, setPlaying] = useState(false)
+  // 播放/暂停按钮
+  const [playIcon, setPlayIcon] = useState('play')
+  // 视频切换
+  const videoList = [
+    '/pageMaterial/video/video_1.mp4',
+    '/pageMaterial/video/video_1.mp4'
+  ]
+  const [videoIndex, setVideoIndex] = useState(0)
+
+  const videoPlayHandler = () => {
+    if (playing) {
+      pauseVideo()
+      setPlayIcon('play')
+    } else {
+      playVideo()
+      setPlayIcon('pause')
+    }
+  }
+
+  //
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.addEventListener('ended', () => {
+        setPlaying(false)
+        setPlayIcon('play')
+      })
+    }
+
+
+  }, [playing])
+
+  // 播放视频
+  const playVideo = () => {
+    if (videoRef.current) {
+      videoRef.current.play()
+      setPlaying(true)
+    }
+  }
+
+  // 暂停视频
+  const pauseVideo = () => {
+    if (videoRef.current) {
+      videoRef.current.pause()
+      setPlaying(false)
+    }
+  }
+
+  // 切换视频
+  const changeVideo = (index) => {
+    if (videoRef.current) {
+      videoRef.current.src = videoList[index]
+      setVideoIndex(index)
+      playVideo()
+      videoPlayHandler()
+    }
+  }
+
   return (
     <VideoWrapper>
-      <h1>Video</h1>
+      <div className="video-wrapper" >
+        <div className="video-content">
+          {/* 视频背景边框 */}
+          <div
+            className="video-bg"
+          >
+            {/* 播放暂停按钮背景 */}
+            <img
+              className="video-control-bg"
+              src="/pageMaterial/img/common/videoBoard/video_control.png"
+              onClick={videoPlayHandler}
+            ></img>
+            {/* 播放暂停按钮 */}
+            <img
+              className="video-control"
+              src={`/pageMaterial/img/common/videoBoard/video_${playIcon}.png`}
+              onClick={videoPlayHandler}
+            ></img>
+          {/* 视频内容 */}
+            <video
+              ref={videoRef}
+              src={videoList[videoIndex]}
+              className="video-viewport"
+            ></video>
+          </div>
+
+        </div>
+          <div className="video-select">
+            {
+              videoList.map((item, index) => {
+                return (
+                  <div
+                    className={`video-select-item ${videoIndex === index ? 'video-select-item-active' : ''}`}
+                    key={index}
+                    onClick={() => changeVideo(index)}
+                  >
+                    <img src="/pageMaterial/img/common/videotape/videotape_1.png" alt="" />
+                  </div>
+                )
+              })
+            }
+            {/* // <div className="video-select-item">
+            //   <img src="/pageMaterial/img/common/videotape/videotape_1.png" alt="" />
+            // </div> */}
+          </div>
+      </div>
     </VideoWrapper>
   )
 })
--- src/views/home/children/video/style.js
+++ src/views/home/children/video/style.js
@@ -1,5 +1,63 @@
 import styled from 'styled-components'
 
-const VideoWrapper = styled.div``
+const VideoWrapper = styled.div`
+  .video-wrapper {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-flow: column nowrap;
+    align-items: center;
+    justify-content: space-around;
+    /* position: relative; */
+    .video-content {
+      .video-bg {
+        background: url('/pageMaterial/img/common/videoBoard/videoBoard.png');
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: center center;
+        background-size: contain;
+        width: 85rem;
+        height: 35rem;
+        position: relative;
+      }
+      .video-control-bg {
+        position: absolute;
+        left: 10.5rem;
+        bottom: 3rem;
+        width: 11rem;
+      }
+      .video-control {
+        position: absolute;
+        left: 14.3rem;
+        bottom: 5rem;
+        width: 4.5rem;
+      }
+      .video-viewport {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -55%);
+        width: 37rem;
+      }
+    }
+    .video-select {
+      width:  85rem;
+      display: flex;
+      flex-flow: row nowrap;
+      align-items: center;
+      justify-content: space-around;
+      box-sizing: border-box;
+      padding: 0 10rem;
+      .video-select-item {
+        img {
+          width: 13rem;
+        }
+      }
+      .video-select-item-active{
+
+      }
+    }
+  }
+`
 
 export default VideoWrapper
