d645ee4571e9491e584acfaa480cbb64f9e897e3
tsja2001 <52786405+tsja2001@users.noreply.github.com>
2023-03-02 03:26:31 +0800

    feat:完成播放功能:1/1

--- src/views/home/children/voice/index.jsx
+++ src/views/home/children/voice/index.jsx
@@ -1,11 +1,94 @@
 import PropTypes from 'prop-types'
-import React, { memo } from 'react'
+import React, { memo, useState } from 'react'
 import VoiceWrapper from './style'
 
-const Voice = memo((props) => {
+const Voice = memo(({ pauseAudioPlay }) => {
+  const [currentPlay, setCurrentPlay] = useState('luoXue')
+  const [isPlaying, setIsPlaying] = useState(false)
+  const [currentTime, setCurrentTime] = useState(0)
+  const [duration, setDuration] = useState(null)
+
+  const audioRef = React.createRef()
+
+  const handleBtn = () => {
+    if (isPlaying) {
+      handlePause()
+    } else {
+      handlePlay()
+    }
+  }
+
+  const handleTimeUpdate = (e) => {
+    setCurrentTime(e.target.currentTime)
+  }
+
+  const handleLoadedMetadata = (e) => {
+    setDuration(e.target.duration)
+  }
+
+  const handlePlay = () => {
+    pauseAudioPlay()
+    setIsPlaying(true)
+    audioRef.current.play()
+  }
+
+  const handlePause = () => {
+    setIsPlaying(false)
+    audioRef.current.pause()
+  }
+
+  const checkedHandler = (name) => {
+    setCurrentPlay(name)
+    handlePlay()
+  }
+
   return (
     <VoiceWrapper>
-      <h1>Voice</h1>
+      <div className="voice-wapper">
+        <div className="left">
+          <div className="play">
+            <img
+              className="play-bg"
+              src="/pageMaterial/img/common/recorde/record.png"
+            ></img>
+            <img
+              className={`play-needle ${!isPlaying ? 'play-needle-pause' : ''}`}
+              src="/pageMaterial/img/common/recorde/recordHand.png"
+            ></img>
+            <img
+              onClick={handleBtn}
+              className="play-icon"
+              src={`/pageMaterial/img/common/recorde/${
+                isPlaying ? 'recordPause' : 'recordPaly'
+              }.png`}
+            ></img>
+          </div>
+          <div className="autio-progress">
+            <audio
+              ref={audioRef}
+              src={`/pageMaterial/voice/voice_${currentPlay}.mp3`}
+              autoPlay={true}
+              onTimeUpdate={handleTimeUpdate}
+              onLoadedMetadata={handleLoadedMetadata}
+            />
+            <progress max={duration} value={currentTime} />
+          </div>
+        </div>
+        <div className="right">
+          <img
+            onClick={() => checkedHandler('luoXue')}
+            className={`right-item first ${
+              currentPlay === 'luoXue' && isPlaying ? 'right-item-highlight' : ''
+            }`}
+            src="/pageMaterial/img/luoXue/record/record-luoXue.png"
+          />
+          <img
+            onClick={() => checkedHandler('jirRan')}
+            className="right-item"
+            src="/pageMaterial/img/jieRan/record/record-jieRan.png"
+          />
+        </div>
+      </div>
     </VoiceWrapper>
   )
 })
--- src/views/home/children/voice/style.js
+++ src/views/home/children/voice/style.js
@@ -1,5 +1,99 @@
 import styled from 'styled-components'
 
-const VoiceWrapper = styled.div``
+const VoiceWrapper = styled.div`
+.voice-wapper{
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: center;
+  justify-content: space-between;
+  .left{
+    display: flex;
+    flex-flow: column nowrap;
+    align-items: center;
+    justify-content: flex-start;
+    flex: 4;
+    height: 100vh;
+    .play{
+      position: relative;
+      width: 100%;
+      .play-needle{
+        position: absolute;
+        top: 6rem;
+        left: 4rem;
+        width: 16rem;
+        transform-origin: top left;
+        transition: transform 0.3s;
+      }
+      .play-needle-pause{
+        transform: rotate(-23deg);
+      }
+      .play-bg{
+        position: absolute;
+        top: 7rem;
+        left: 3rem;
+        width: 23rem;
+      }
+      .play-icon{
+        position: absolute;
+        top: 18.5rem;
+        left: 13.6rem;
+        width: 2rem;
+      }
+    }
+    .autio-progress{
+      position: absolute;
+      top: 35rem;
+      left: 8rem;
+      left: 3rem;
+      width: 23rem;
+      progress{
+        width: 23rem;
+      }
+      /* font-size: 2.5rem; */
+    }
+  }
+  .right{
+    flex: 6;
+    display: flex;
+    flex-flow: column wrap;
+    align-items: center;
+    height: 60vh;
+    justify-content: space-between;
+    .right-item{
+      width: 250px;
+      transition: transform 0.3s ease-out, margin-top 0.3s ease-out;
+        transform-origin: top center;
+    }
+    .right-item:hover {
+      transform: scale(1.05);
+      margin-bottom: -10px;
+    }
+
+    .right-item-highlight {
+      filter: brightness(190%);
+      animation: pulse 1s ease-in-out infinite alternate;
+    }
+
+    @keyframes pulse {
+    from {
+      transform: scale(1);
+    }
+    to {
+      transform: scale(1.03);
+    }
+  }
+  }
+
+  progress::-webkit-progress-value {
+    background-color: green;
+  }
+  /* Mozilla Firefox */
+  progress::-moz-progress-bar {
+    background-color: green;
+  }
+}
+`
 
 export default VoiceWrapper
